430e427baa30135dabb375d58d16c160c50843cd
Alexia <alexiam605@gmail.com>
2026-02-12 20:06:34 -0500

    Add Assignment 2 producer-consumer implementation

---
+++ assignment2/buffer.h
@@ -0,0 +1,35 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#define _GNU_SOURCE
+#include <stddef.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+
+#define BUFFER_SIZE 10
+#define SHM_KEY 0x1234
+#define SEM_MUTEX "/sem_mutex"
+#define SEM_EMPTY "/sem_empty"
+#define SEM_FULL "/sem_full"
+
+typedef struct {
+   int value;
+   int producer_id;
+} item_t;
+
+typedef struct {
+   item_t buffer[BUFFER_SIZE];
+   int head;
+   int tail;
+   int count;
+} shared_buffer_t;
+
+#endif
